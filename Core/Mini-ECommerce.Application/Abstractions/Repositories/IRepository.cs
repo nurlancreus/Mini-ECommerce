4b984800fe16daba73993dd1529517f989684a40
nurlancreus <nurlancreus007@gmail.com>
2024-08-10 17:17:41 +0400

    adding repositories abstractions

---
+++ Core/Mini-ECommerce.Application/Abstractions/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mini_ECommerce.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Abstractions.Repositories
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        DbSet<T> Table { get; }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/Abstractions/Repositories/IWriteRepository.cs
@@ -0,0 +1,20 @@
+﻿using Mini_ECommerce.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Abstractions.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        Task<bool> AddAsync(T entity);
+        Task<bool> AddRangeAsync(List<T> entities);
+        bool Remove(T? entity);
+        bool RemoveRange(List<T> entities);
+        Task<bool> RemoveAsync(string id);
+        bool Update(T entity);
+        Task<int> SaveAsync();
+    }
+}
