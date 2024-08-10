a1d9a22d181e097966756b61504ad677296b76b6
nurlancreus <nurlancreus007@gmail.com>
2024-08-10 16:25:16 +0400

    adding context

---
+++ Infrastructure/Mini-ECommerce.Persistence/Contexts/MiniECommerceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mini_ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Persistence.Contexts
+{
+    public class MiniECommerceDbContext(DbContextOptions<MiniECommerceDbContext> contextOptions) : DbContext(contextOptions)
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
