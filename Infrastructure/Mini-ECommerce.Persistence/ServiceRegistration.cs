00a803b40f2ef04a7c685d0990066816d8e4b0ee
nurlancreus <nurlancreus007@gmail.com>
2024-08-10 20:56:55 +0400

    adding persistence repository services to the IoC

--- Infrastructure/Mini-ECommerce.Persistence/ServiceRegistration.cs
+++ Infrastructure/Mini-ECommerce.Persistence/ServiceRegistration.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Mini_ECommerce.Application.Abstractions.Repositories.Customer;
+using Mini_ECommerce.Application.Abstractions.Repositories.Order;
+using Mini_ECommerce.Application.Abstractions.Repositories.Product;
+using Mini_ECommerce.Persistence.Concretes.Repositories.Customer;
+using Mini_ECommerce.Persistence.Concretes.Repositories.Order;
+using Mini_ECommerce.Persistence.Concretes.Repositories.Product;
 using Mini_ECommerce.Persistence.Contexts;
 using System;
 using System.Collections.Generic;
@@ -15,6 +21,13 @@ namespace Mini_ECommerce.Persistence
         public static void AddPersistenceServices (this IServiceCollection services)
         {
             services.AddDbContext<MiniECommerceDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
         }
     }
 }
