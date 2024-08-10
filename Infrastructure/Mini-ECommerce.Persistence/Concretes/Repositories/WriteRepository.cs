efff1181a67f314db3d8e7df8c8b7841bf0539fb
nurlancreus <nurlancreus007@gmail.com>
2024-08-10 17:17:59 +0400

    adding repositories concretes

---
+++ Infrastructure/Mini-ECommerce.Persistence/Concretes/Repositories/WriteRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using Mini_ECommerce.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mini_ECommerce.Persistence.Contexts;
+using Mini_ECommerce.Application.Abstractions.Repositories;
+
+namespace Mini_ECommerce.Persistence.Concretes.Repositories
+{
+    public class WriteRepository<T> : IWriteRepository<T> where T : BaseEntity
+    {
+        readonly private MiniECommerceDbContext _context;
+        public WriteRepository(MiniECommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+        public async Task<bool> AddAsync(T entity)
+        {
+            EntityEntry<T> entityEntry = await Table.AddAsync(entity);
+            return entityEntry.State == EntityState.Added;
+        }
+        public async Task<bool> AddRangeAsync(List<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+            return true;
+        }
+        public bool Remove(T? entity)
+        {
+            if (entity != null)
+            {
+                EntityEntry<T> entityEntry = Table.Remove(entity);
+                return entityEntry.State == EntityState.Deleted;
+            }
+
+            else return false;
+        }
+        public bool RemoveRange(List<T> entities)
+        {
+            Table.RemoveRange(entities);
+            return true;
+        }
+        public async Task<bool> RemoveAsync(string id)
+        {
+            T? entity = await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+
+            return Remove(entity);
+        }
+        public bool Update(T entity)
+        {
+            EntityEntry entityEntry = Table.Update(entity);
+            return entityEntry.State == EntityState.Modified;
+        }
+        public async Task<int> SaveAsync()
+            => await _context.SaveChangesAsync();
+    }
+}
