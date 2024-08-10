11b1be0d8f2f759483573308c4319208a3ecc43d
nurlancreus <nurlancreus007@gmail.com>
2024-08-10 14:32:51 +0400

    Add project files.

---
+++ Core/Mini-ECommerce.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using Mini_ECommerce.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = "";
+        public int Stock { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ Infrastructure/Mini-ECommerce.Persistence/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices (this IServiceCollection services)
+        {
+
+        }
+    }
+}
