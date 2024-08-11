485caba850ee3cda09fe11fff8c444c790ae47e1
nurlancreus <nurlancreus007@gmail.com>
2024-08-11 18:57:20 +0400

    adding create and update product validators

---
+++ Core/Mini-ECommerce.Application/Validators/Product/CreateProductValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using Mini_ECommerce.Application.ViewModels.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Validators.Product
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductVM>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                    .WithMessage("Please do not leave the product name empty.")
+                .NotNull()
+                    .WithMessage("Product name is required.")
+                .Length(5, 150)
+                    .WithMessage("The product name must be between 5 and 150 characters.")
+                .Matches(@"^[a-zA-Z0-9\s]*$")
+                    .WithMessage("The product name can only contain letters, numbers, and spaces.");
+
+            RuleFor(p => p.Stock)
+                .NotNull()
+                    .WithMessage("Stock information is required.")
+                .GreaterThanOrEqualTo(0)
+                    .WithMessage("Stock cannot be negative.")
+                .LessThanOrEqualTo(10000)
+                    .WithMessage("Stock cannot exceed 10,000 units.");
+
+            RuleFor(p => p.Price)
+                .NotNull()
+                    .WithMessage("Price information is required.")
+                .GreaterThanOrEqualTo(0.0f)
+                    .WithMessage("Price cannot be negative.");
+        }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/Validators/Product/UpdateProductValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using Mini_ECommerce.Application.ViewModels.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Validators.Product
+{
+    public class UpdateProductValidator : AbstractValidator<UpdateProductVM>
+    {
+        public UpdateProductValidator()
+        {
+            RuleFor(p => p.Name)
+               .NotEmpty()
+                   .WithMessage("Please do not leave the product name empty.")
+               .NotNull()
+                   .WithMessage("Product name is required.")
+               .Length(5, 150)
+                   .WithMessage("The product name must be between 5 and 150 characters.");
+
+            RuleFor(p => p.Stock)
+                .NotNull()
+                    .WithMessage("Stock information is required.")
+                .GreaterThanOrEqualTo(0)
+                    .WithMessage("Stock cannot be negative.");
+
+            RuleFor(p => p.Price)
+                .NotNull()
+                    .WithMessage("Price information is required.")
+                .GreaterThanOrEqualTo(0.0f)
+                    .WithMessage("Price cannot be negative.")
+                    .WithMessage("Price must have up to 2 decimal places.");
+        }
+    }
+}
