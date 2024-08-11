2bffc596cf772bd27f14af8aaea99a9e7067569c
nurlancreus <nurlancreus007@gmail.com>
2024-08-11 18:55:55 +0400

    adding products controller

---
+++ Presentation/Mini-ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mini_ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    
+    public class ProductsController : ControllerBase
+    {
+    }
+}
