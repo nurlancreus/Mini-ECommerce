286974f01265eb7c8f217bb5139d539066a51ca9
nurlancreus <nurlancreus007@gmail.com>
2024-08-16 00:13:48 +0400

    adding storage service abstractions

---
+++ Core/Mini-ECommerce.Application/Abstractions/Services/Storage/Local/ILocalStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Mini_ECommerce.Application.Abstractions.Services.Storage;
+
+namespace Mini_ECommerce.Application.Abstractions.Services.Storage.Local
+{
+    public interface ILocalStorage : IStorage
+    {
+        //protected Task<bool> CopyFileAsync(string path, IFormFile formFile);
+    }
+}
