8cc08bec6b3b9d589b0ab5c77dc38f89b64ecbe2
nurlancreus <nurlancreus007@gmail.com>
2024-08-16 23:15:41 +0400

    azure storage naming

--- Infrastructure/Mini-ECommerce.Infrastructure/Concretes/Services/Storage/Azure/AzureStorage.cs
+++ Infrastructure/Mini-ECommerce.Infrastructure/Concretes/Services/Storage/Azure/AzureStorage.cs
@@ -11,22 +11,22 @@ namespace Mini_ECommerce.Infrastructure.Concretes.Services.Storage.Azure
 {
     public class AzureStorage : IAzureStorage
     {
-        public Task DeleteAsync(string pathOrContainerName, string fileName)
+        public Task DeleteAsync(string containerName, string fileName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string pathOrContainerName, IFormFileCollection formFiles)
+        public Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string containerName, IFormFileCollection formFiles)
         {
             throw new NotImplementedException();
         }
 
-        Task<List<string>> IStorage.GetFilesAsync(string pathOrContainerName)
+        Task<List<string>> IStorage.GetFilesAsync(string containerName)
         {
             throw new NotImplementedException();
         }
 
-        Task<bool> IStorage.HasFileAsync(string pathOrContainerName, string fileName)
+        Task<bool> IStorage.HasFileAsync(string containerName, string fileName)
         {
             throw new NotImplementedException();
         }
