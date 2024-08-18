d2880a6e73b2a325b1dc068382dfbe506b777a2e
nurlancreus <nurlancreus007@gmail.com>
2024-08-18 14:11:08 +0400

    register new mediatr assembly in application service registration

---
+++ Core/Mini-ECommerce.Application/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServiceRegistration).Assembly));
+        }
+    }
+}
