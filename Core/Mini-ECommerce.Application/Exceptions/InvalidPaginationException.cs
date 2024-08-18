6e28a22fbcea21ae5895b28ba39f31af1c2a8766
nurlancreus <nurlancreus007@gmail.com>
2024-08-18 14:09:38 +0400

    adding pagination related custom exceptions class

---
+++ Core/Mini-ECommerce.Application/Exceptions/InvalidPaginationException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Exceptions
+{
+    public enum PaginationErrorType
+    {
+        InvalidPageSize,
+        InvalidPageNumber
+    }
+
+    public class InvalidPaginationException : Exception
+    {
+        public PaginationErrorType ErrorType { get; }
+        public int InvalidValue { get; }
+
+        public InvalidPaginationException(PaginationErrorType errorType, int invalidValue)
+            : base(GenerateMessage(errorType, invalidValue))
+        {
+            ErrorType = errorType;
+            InvalidValue = invalidValue;
+        }
+
+        private static string GenerateMessage(PaginationErrorType errorType, int invalidValue)
+        {
+            return errorType switch
+            {
+                PaginationErrorType.InvalidPageSize => $"Invalid page size: {invalidValue}. Page size must be a positive number and cannot exceed 100.",
+                PaginationErrorType.InvalidPageNumber => $"Invalid page number: {invalidValue}.",
+                _ => "Invalid pagination parameter."
+            };
+        }
+    }
+}
