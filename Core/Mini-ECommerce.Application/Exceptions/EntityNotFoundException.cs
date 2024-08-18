89e71521cb4270c9ad0580a336f32b115ec1d52b
nurlancreus <nurlancreus007@gmail.com>
2024-08-18 16:56:40 +0400

    adding custom exceptions folder with pagination & entitynotfound exceptions

---
+++ Core/Mini-ECommerce.Application/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public string EntityName { get; }
+
+        public EntityNotFoundException(string entityName)
+            : base($"The entity '{entityName}' was not found.")
+        {
+            EntityName = entityName;
+        }
+
+        public EntityNotFoundException(string entityName, string customMessage)
+            : base(customMessage)
+        {
+            EntityName = entityName;
+        }
+
+        public EntityNotFoundException(string entityName, int entityId)
+            : base($"The entity '{entityName}' with ID '{entityId}' was not found.")
+        {
+            EntityName = entityName;
+        }
+
+        public EntityNotFoundException(string entityName, int entityId, string customMessage)
+            : base(customMessage)
+        {
+            EntityName = entityName;
+        }
+    }
+}
