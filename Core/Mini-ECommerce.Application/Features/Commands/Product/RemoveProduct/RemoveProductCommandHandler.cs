a7c626f371c18b9408803b612016de6a15d4a0e5
nurlancreus <nurlancreus007@gmail.com>
2024-08-19 14:20:18 +0400

    adding new remove product command

---
+++ Core/Mini-ECommerce.Application/Features/Commands/Product/RemoveProduct/RemoveProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Mini_ECommerce.Application.Abstractions.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Commands.Product.RemoveProduct
+{
+    public class RemoveProductCommandHandler : IRequestHandler<RemoveProductCommandRequest, RemoveProductCommandResponse>
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+
+        public RemoveProductCommandHandler(IProductWriteRepository productWriteRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+        }
+
+        public async Task<RemoveProductCommandResponse> Handle(RemoveProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            bool isDeleted = await _productWriteRepository.RemoveAsync(request.Id);
+
+            if (!isDeleted)
+            {
+                throw new Exception("Could not delete product");
+            }
+
+            return new RemoveProductCommandResponse();
+        }
+    }
+}
