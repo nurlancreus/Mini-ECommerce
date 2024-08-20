61a9620f19069c7e926c06ee6c9c6cba69914596
nurlancreus <nurlancreus007@gmail.com>
2024-08-20 23:24:06 +0400

    validation loginuser command

---
+++ Core/Mini-ECommerce.Application/Validators/AppUser/LoginUserCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Mini_ECommerce.Application.Features.Commands.AppUser.LoginUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Validators.AppUser
+{
+    public class LoginUserCommandValidator : AbstractValidator<LoginUserCommandRequest>
+    {
+        public LoginUserCommandValidator()
+        {
+            RuleFor(u => u.Email)
+                .NotEmpty().WithMessage("Username or Email is required.")
+                .NotNull().WithMessage("Username or Email cannot be null.")
+                .EmailAddress().WithMessage("Please enter a valid email address or username.");
+            
+            RuleFor(u => u.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .NotNull().WithMessage("Password cannot be null.")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+        }
+    }
+}
