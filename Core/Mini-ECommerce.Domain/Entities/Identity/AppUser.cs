ff21d3c8546e3e238798e97ec61e00ba23a2a841
nurlancreus <nurlancreus007@gmail.com>
2024-08-20 20:39:01 +0400

    adding identity entities

---
+++ Core/Mini-ECommerce.Domain/Entities/Identity/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Domain.Entities.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
