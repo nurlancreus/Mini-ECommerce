6ec7d4b7c1ea9dcd88ef259fdbf841ee499d649e
nurlancreus <nurlancreus007@gmail.com>
2024-08-22 00:01:29 +0400

    adding facebookaccessDTOs

---
+++ Core/Mini-ECommerce.Application/DTOs/FacebookAccess/FacebookUserInfoResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.FacebookAccess
+{
+    public class FacebookUserInfoResponseDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+        
+        [JsonPropertyName("last_name")]
+        public string LastName { get; set; }
+    }
+}
