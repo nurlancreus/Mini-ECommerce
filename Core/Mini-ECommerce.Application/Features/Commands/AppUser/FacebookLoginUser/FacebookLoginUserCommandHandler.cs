49b071168423b7b6280dcfe7429dc4bcf301aeec
nurlancreus <nurlancreus007@gmail.com>
2024-08-21 14:03:16 +0400

    configuring setup for facebook login commands

---
+++ Core/Mini-ECommerce.Application/Features/Commands/AppUser/FacebookLoginUser/FacebookLoginUserCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Commands.AppUser.FacebookLoginUser
+{
+    public class FacebookLoginUserCommandHandler : IRequestHandler<FacebookLoginUserCommandRequest, FacebookLoginUserCommandResponse>
+    {
+        public Task<FacebookLoginUserCommandResponse> Handle(FacebookLoginUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/Features/Commands/AppUser/FacebookLoginUser/FacebookLoginUserCommandRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Commands.AppUser.FacebookLoginUser
+{
+    public class FacebookLoginUserCommandRequest : IRequest<FacebookLoginUserCommandResponse>
+    {
+    }
+}
