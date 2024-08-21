317c6eb5498e6e320692f1454448c217b64e131b
nurlancreus <nurlancreus007@gmail.com>
2024-08-22 00:00:35 +0400

    modify userscontroller

--- Presentation/Mini-ECommerce.API/Controllers/UsersController.cs
+++ Presentation/Mini-ECommerce.API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Mini_ECommerce.Application.Exceptions;
+using Mini_ECommerce.Application.Features.Commands.AppUser.FacebookLoginUser;
 using Mini_ECommerce.Application.Features.Commands.AppUser.GoogleLoginUser;
 using Mini_ECommerce.Application.Features.Commands.AppUser.LoginUser;
 using Mini_ECommerce.Application.Features.Commands.AppUser.RegisterUser;
@@ -61,6 +62,24 @@ namespace Mini_ECommerce.API.Controllers
             try
             {
                 var response = await _mediator.Send(googleLoginUserCommandRequest);
+                return Ok(response);
+            }
+            catch (LoginException ex)
+            {
+                return StatusCode((int)ex.StatusCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = "An unexpected error occurred.", Details = ex.Message });
+            }
+        }
+        
+        [HttpPost("login-facebook")]
+        public async Task<IActionResult> FacebookLogin(FacebookLoginUserCommandRequest facebookLoginUserCommandRequest)
+        {
+            try
+            {
+                var response = await _mediator.Send(facebookLoginUserCommandRequest);
                 return Ok(response);
             }
             catch (LoginException ex)
