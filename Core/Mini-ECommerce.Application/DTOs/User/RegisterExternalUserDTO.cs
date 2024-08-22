18c79dc8022216d2d2668ca3745353a9f02e0390
nurlancreus <nurlancreus007@gmail.com>
2024-08-22 13:17:26 +0400

    adding new DTO classes

---
+++ Core/Mini-ECommerce.Application/DTOs/User/RegisterExternalUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Mini_ECommerce.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.User
+{
+    public class RegisterExternalUserDTO
+    {
+        public AppUser User { get; set; }
+        public UserLoginInfo UserInfo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
