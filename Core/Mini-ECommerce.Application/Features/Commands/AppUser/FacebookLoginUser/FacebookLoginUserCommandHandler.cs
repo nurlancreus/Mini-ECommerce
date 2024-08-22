5f02218d0b1a7f6211173531f6682bbc17e14e06
nurlancreus <nurlancreus007@gmail.com>
2024-08-22 13:19:01 +0400

    implementing externalauth services to the cqrs pattern

--- Core/Mini-ECommerce.Application/Features/Commands/AppUser/FacebookLoginUser/FacebookLoginUserCommandHandler.cs
+++ Core/Mini-ECommerce.Application/Features/Commands/AppUser/FacebookLoginUser/FacebookLoginUserCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Mini_ECommerce.Application.Abstractions.Services.Auth;
 using Mini_ECommerce.Application.Abstractions.Services.Token;
 using Mini_ECommerce.Application.DTOs.FacebookAccess;
 using Mini_ECommerce.Application.DTOs.Token;
@@ -18,71 +19,17 @@ namespace Mini_ECommerce.Application.Features.Commands.AppUser.FacebookLoginUser
 {
     public class FacebookLoginUserCommandHandler : IRequestHandler<FacebookLoginUserCommandRequest, FacebookLoginUserCommandResponse>
     {
-        private readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
-        private readonly HttpClient _httpClient;
-        private readonly IAppTokenHandler _tokenHandler;
-        private readonly IConfiguration _configuration;
+        private readonly IExternalAuthService _externalAuthService;
 
-        public FacebookLoginUserCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager, IHttpClientFactory httpClientFactory, IConfiguration configuration, IAppTokenHandler tokenHandler)
+        public FacebookLoginUserCommandHandler(IExternalAuthService externalAuthService)
         {
-            _userManager = userManager;
-            _httpClient = httpClientFactory.CreateClient();
-            _configuration = configuration;
-            _tokenHandler = tokenHandler;
+            _externalAuthService = externalAuthService;
         }
 
         public async Task<FacebookLoginUserCommandResponse> Handle(FacebookLoginUserCommandRequest request, CancellationToken cancellationToken)
         {
-            string accessTokenResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/oauth/access_token?client_id={_configuration["ExternalLoginSettings:Facebook:AppId"]}&client_secret={_configuration["ExternalLoginSettings:Facebook:AppId"]}&grant_type=client_credentials");
 
-            var facebookAccessToken = JsonSerializer.Deserialize<FacebookAccessTokenResponseDTO>(accessTokenResponse);
-
-            string userAccessTokenValidation = await _httpClient.GetStringAsync($"https://graph.facebook.com/debug_token?input_token={request.AuthToken}&access_token={facebookAccessToken}");
-
-            var validation = JsonSerializer.Deserialize<FacebookUserAccessTokenValidationDTO>(userAccessTokenValidation);
-
-            if (validation?.Data.IsValid == null)
-            {
-                throw new LoginException("Invalid external authentication.");
-
-            }
-            string userInfoResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/me?fields=email,first_name,last_name&access_token={request.AuthToken}");
-
-            var userInfo = JsonSerializer.Deserialize<FacebookUserInfoResponseDTO>(userInfoResponse);
-
-            var info = new UserLoginInfo(ExternalLoginProvider.Facebook.ToString(), validation.Data.UserId, ExternalLoginProvider.Facebook.ToString());
-
-            var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
-
-            bool result = user != null;
-
-            if (user == null)
-            {
-                user = await _userManager.FindByEmailAsync(userInfo.Email);
-
-                if (user == null)
-                {
-                    user = new()
-                    {
-                        Email = userInfo.Email,
-                        UserName =userInfo.Email,
-                        FirstName = userInfo.FirstName,
-                        LastName = userInfo.LastName,
-                    };
-
-                    var identityResult = await _userManager.CreateAsync(user);
-                    result = identityResult.Succeeded;
-                }
-            }
-
-            if (!result)
-            {
-                throw new Exception("Invalid external authentication.");
-            }
-
-            await _userManager.AddLoginAsync(user, info); //AspNetUserLogins
-
-            TokenDTO token = _tokenHandler.CreateAccessToken(900, user);
+            var token = await _externalAuthService.FacebookLoginAsync(request.AuthToken, ExternalLoginProvider.Facebook.ToString(), 10000);
 
             return new FacebookLoginUserCommandResponse()
             {
--- Core/Mini-ECommerce.Application/Features/Commands/AppUser/GoogleLoginUser/GoogleLoginUserCommandHandler.cs
+++ Core/Mini-ECommerce.Application/Features/Commands/AppUser/GoogleLoginUser/GoogleLoginUserCommandHandler.cs
@@ -1,11 +1,10 @@
-﻿using Google.Apis.Auth;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Mini_ECommerce.Application.Abstractions.Services.Auth;
 using Mini_ECommerce.Application.Abstractions.Services.Token;
 using Mini_ECommerce.Application.DTOs.Token;
 using Mini_ECommerce.Domain.Enums;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,59 +16,17 @@ namespace Mini_ECommerce.Application.Features.Commands.AppUser.GoogleLoginUser
 {
     public class GoogleLoginUserCommandHandler : IRequestHandler<GoogleLoginUserCommandRequest, GoogleLoginUserCommandResponse>
     {
-        private readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
-        private readonly IAppTokenHandler _tokenHandler;
-        private readonly IConfiguration _configuration;
+        private readonly IExternalAuthService _externalAuthService;
 
-        public GoogleLoginUserCommandHandler(UserManager<Domain.Entities.Identity.AppUser> usermanager, IConfiguration configuration, IAppTokenHandler tokenHandler)
+        public GoogleLoginUserCommandHandler(IExternalAuthService externalAuthService)
         {
-            _userManager = usermanager;
-            _configuration = configuration;
-            _tokenHandler = tokenHandler;
+            _externalAuthService = externalAuthService;
         }
 
         public async Task<GoogleLoginUserCommandResponse> Handle(GoogleLoginUserCommandRequest request, CancellationToken cancellationToken)
         {
-            var settings = new GoogleJsonWebSignature.ValidationSettings()
-            {
-                Audience = [_configuration["ExternalLoginSettings:Google:ClientId"]]
-            };
 
-           var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken, settings);
-
-            var info = new UserLoginInfo(ExternalLoginProvider.Google.ToString(), payload.Subject, ExternalLoginProvider.Google.ToString());
-
-            var user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
-
-            bool result = user != null;
-
-            if (user == null)
-            {
-                user = await _userManager.FindByEmailAsync(payload.Email);
-
-                if (user == null)
-                {
-                    user = new()
-                    {
-                        Email = payload.Email,
-                        UserName = payload.Email,
-                        FirstName = payload.GivenName,
-                        LastName = payload.FamilyName
-                    };
-
-                    var identityResult = await _userManager.CreateAsync(user);
-                    result = identityResult.Succeeded;
-                }
-            }
-
-            if (!result)
-            {
-                throw new Exception("Invalid external authentication.");
-            }
-
-            await _userManager.AddLoginAsync(user, info); //AspNetUserLogins
-
-            TokenDTO token = _tokenHandler.CreateAccessToken(900, user);
+            var token = await _externalAuthService.GoogleLoginAsync(request.IdToken, ExternalLoginProvider.Google.ToString(), 10000);
 
             return new GoogleLoginUserCommandResponse()
             {
