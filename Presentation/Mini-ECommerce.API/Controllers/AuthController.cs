329f7e5fdfd973b8ace04e030d3ca8d5527a24db
nurlancreus <nurlancreus007@gmail.com>
2024-08-22 13:20:06 +0400

    add new authcontroller and seperating auth operations from userscontroller

---
+++ Presentation/Mini-ECommerce.API/Controllers/AuthController.cs
@@ -0,0 +1,77 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mini_ECommerce.Application.Exceptions;
+using Mini_ECommerce.Application.Features.Commands.AppUser.FacebookLoginUser;
+using Mini_ECommerce.Application.Features.Commands.AppUser.GoogleLoginUser;
+using Mini_ECommerce.Application.Features.Commands.AppUser.LoginUser;
+using Mini_ECommerce.Application.Features.Commands.AppUser.RegisterUser;
+
+namespace Mini_ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginUserCommandRequest loginUserCommandRequest)
+        {
+            try
+            {
+                var response = await _mediator.Send(loginUserCommandRequest);
+                return Ok(response);
+            }
+            catch (LoginException ex)
+            {
+                return StatusCode((int)ex.StatusCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = "An unexpected error occurred.", Details = ex.Message });
+            }
+        }
+
+        [HttpPost("login-google")]
+        public async Task<IActionResult> GoogleLogin(GoogleLoginUserCommandRequest googleLoginUserCommandRequest)
+        {
+            try
+            {
+                var response = await _mediator.Send(googleLoginUserCommandRequest);
+                return Ok(response);
+            }
+            catch (LoginException ex)
+            {
+                return StatusCode((int)ex.StatusCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = "An unexpected error occurred.", Details = ex.Message });
+            }
+        }
+
+        [HttpPost("login-facebook")]
+        public async Task<IActionResult> FacebookLogin(FacebookLoginUserCommandRequest facebookLoginUserCommandRequest)
+        {
+            try
+            {
+                var response = await _mediator.Send(facebookLoginUserCommandRequest);
+                return Ok(response);
+            }
+            catch (LoginException ex)
+            {
+                return StatusCode((int)ex.StatusCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Error = "An unexpected error occurred.", Details = ex.Message });
+            }
+        }
+    }
+}
--- Presentation/Mini-ECommerce.API/Controllers/UsersController.cs
+++ Presentation/Mini-ECommerce.API/Controllers/UsersController.cs
@@ -37,59 +37,5 @@ namespace Mini_ECommerce.API.Controllers
                 return BadRequest(new { Error = "An unexpected error occurred.", Details = ex.Message });
             }
         }
-
-        [HttpPost("login")]
-        public async Task<IActionResult> Login(LoginUserCommandRequest loginUserCommandRequest)
-        {
-            try
-            {
-                var response = await _mediator.Send(loginUserCommandRequest);
-                return Ok(response);
-            }
-            catch (LoginException ex)
-            {
-                return StatusCode((int)ex.StatusCode, ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { Error = "An unexpected error occurred.", Details = ex.Message });
-            }
-        }
-
-        [HttpPost("login-google")]
-        public async Task<IActionResult> GoogleLogin(GoogleLoginUserCommandRequest googleLoginUserCommandRequest)
-        {
-            try
-            {
-                var response = await _mediator.Send(googleLoginUserCommandRequest);
-                return Ok(response);
-            }
-            catch (LoginException ex)
-            {
-                return StatusCode((int)ex.StatusCode, ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { Error = "An unexpected error occurred.", Details = ex.Message });
-            }
-        }
-        
-        [HttpPost("login-facebook")]
-        public async Task<IActionResult> FacebookLogin(FacebookLoginUserCommandRequest facebookLoginUserCommandRequest)
-        {
-            try
-            {
-                var response = await _mediator.Send(facebookLoginUserCommandRequest);
-                return Ok(response);
-            }
-            catch (LoginException ex)
-            {
-                return StatusCode((int)ex.StatusCode, ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { Error = "An unexpected error occurred.", Details = ex.Message });
-            }
-        }
     }
 }
