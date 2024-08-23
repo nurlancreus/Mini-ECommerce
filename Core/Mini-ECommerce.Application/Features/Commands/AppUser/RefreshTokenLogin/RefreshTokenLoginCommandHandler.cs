28d9c5a4fde88f03cd5592295ffc6cf87ec62a3a
nurlancreus <nurlancreus007@gmail.com>
2024-08-23 12:14:03 +0400

    adding refresh token cqrs pattern

---
+++ Core/Mini-ECommerce.Application/Features/Commands/AppUser/RefreshTokenLogin/RefreshTokenLoginCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Mini_ECommerce.Application.Abstractions.Services.Auth;
+using Mini_ECommerce.Application.DTOs.Token;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Commands.AppUser.RefreshTokenLogin
+{
+    public class RefreshTokenLoginCommandHandler : IRequestHandler<RefreshTokenLoginCommandRequest, RefreshTokenLoginCommandResponse>
+    {
+        readonly IInternalAuthService _internalAuthService;
+
+        public RefreshTokenLoginCommandHandler(IInternalAuthService internalAuthService)
+        {
+            _internalAuthService = internalAuthService;
+        }
+
+        public async Task<RefreshTokenLoginCommandResponse> Handle(RefreshTokenLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            TokenDTO token = await _internalAuthService.RefreshTokenLoginAsync(request.RefreshToken);
+
+            return new()
+            {
+                Token = token
+            };
+        }
+    }
+}
