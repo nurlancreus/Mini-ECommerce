73743f8dd4ac1dc6279d2faa1ba856281e5bb853
nurlancreus <nurlancreus007@gmail.com>
2024-08-24 21:11:22 +0400

    adding product hub service concrete and receifefunction names static class

---
+++ Infrastructure/Mini-ECommerce.SignalR/HubServices/ProductHubService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Mini_ECommerce.Application.Abstractions.Hubs;
+using Mini_ECommerce.SignalR.Constants;
+using Mini_ECommerce.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.SignalR.HubServices
+{
+    public class ProductHubService : IProductHubService
+    {
+        readonly IHubContext<ProductHub> _hubContext;
+
+        public ProductHubService(IHubContext<ProductHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task ProductAddedMessageAsync(string message)
+        {
+            await _hubContext.Clients.All.SendAsync(FunctionNames.ProductAddedMessage, message);
+        }
+    }
+}
