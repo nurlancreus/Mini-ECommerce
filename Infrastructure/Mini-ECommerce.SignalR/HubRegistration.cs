346435b42c58b56895269ad691ca48ee526d3f6f
nurlancreus <nurlancreus007@gmail.com>
2024-08-24 21:11:45 +0400

    implement signalR service and hub registration classes

---
+++ Infrastructure/Mini-ECommerce.SignalR/HubRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Mini_ECommerce.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.SignalR
+{
+    public static class HubRegistration
+    {
+        public static void MapHubs(this WebApplication webApplication)
+        {
+            webApplication.MapHub<ProductHub>("/products-hub");
+            // webApplication.MapHub<OrderHub>("/orders-hub");
+        }
+    }
+}
---
+++ Infrastructure/Mini-ECommerce.SignalR/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Mini_ECommerce.Application.Abstractions.Hubs;
+using Mini_ECommerce.SignalR.HubServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.SignalR
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddSignalRServices(this IServiceCollection services)
+        {
+            services.AddTransient<IProductHubService, ProductHubService>();
+            services.AddSignalR();
+
+            return services;
+        }
+    }
+}
