11a4dd436d0aebf7f93943430f1a9cae6e58dc5d
nurlancreus <nurlancreus007@gmail.com>
2024-08-24 21:12:09 +0400

    modify createproductcommandhandler class

--- Core/Mini-ECommerce.Application/Features/Commands/Product/CreateProduct/CreateProductCommandHandler.cs
+++ Core/Mini-ECommerce.Application/Features/Commands/Product/CreateProduct/CreateProductCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Mini_ECommerce.Application.Abstractions.Hubs;
 using Mini_ECommerce.Application.Abstractions.Repositories;
 using System;
 using System.Collections.Generic;
@@ -12,10 +13,12 @@ namespace Mini_ECommerce.Application.Features.Commands.Product.CreateProduct
     public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest, CreateProductCommandResponse>
     {
         private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductHubService _productHubService;
 
-        public CreateProductCommandHandler(IProductWriteRepository productWriteRepository)
+        public CreateProductCommandHandler(IProductWriteRepository productWriteRepository, IProductHubService productHubService)
         {
             _productWriteRepository = productWriteRepository;
+            _productHubService = productHubService;
         }
 
         public async Task<CreateProductCommandResponse> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
@@ -28,6 +31,7 @@ namespace Mini_ECommerce.Application.Features.Commands.Product.CreateProduct
             }
 
             await _productWriteRepository.SaveAsync();
+            await _productHubService.ProductAddedMessageAsync($"Product {request.Name} added.");
 
 
             var response = new CreateProductCommandResponse() { Message = "Product Created Successfully!", };
