baad3fb65edf1a9d167345c285d271bf56ca3a8d
nurlancreus <nurlancreus007@gmail.com>
2024-08-25 21:56:15 +0400

    implement abstract basket repos concretes

---
+++ Infrastructure/Mini-ECommerce.Persistence/Concretes/Repositories/BasketItem/BasketItemReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using Mini_ECommerce.Application.Abstractions.Repositories;
+using Mini_ECommerce.Domain.Entities;
+using Mini_ECommerce.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Persistence.Concretes.Repositories
+{
+    public class BasketItemReadRepository : ReadRepository<BasketItem>, IBasketItemReadRepository
+    {
+        public BasketItemReadRepository(MiniECommerceDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Infrastructure/Mini-ECommerce.Persistence/Concretes/Repositories/BasketItem/BasketItemWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Mini_ECommerce.Application.Abstractions.Repositories;
+using Mini_ECommerce.Domain.Entities;
+using Mini_ECommerce.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Persistence.Concretes.Repositories
+{
+    public class BasketItemWriteRepository : WriteRepository<BasketItem>, IBasketItemWriteRepository
+    {
+        public BasketItemWriteRepository(MiniECommerceDbContext context) : base(context)
+        {
+        }
+    }
+}
