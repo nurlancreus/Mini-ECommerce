3792a9455e898b90fdd46755206fa5c7ad60b4b0
nurlancreus <nurlancreus007@gmail.com>
2024-08-25 16:19:39 +0400

    add new migrations

---
+++ Infrastructure/Mini-ECommerce.Persistence/Migrations/20240825114950_mig_9.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mini_ECommerce.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductProductImageFile");
+
+            migrationBuilder.CreateTable(
+                name: "ProductProductImageFiles",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductImageFileId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsMain = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductProductImageFiles", x => new { x.ProductId, x.ProductImageFileId });
+                    table.ForeignKey(
+                        name: "FK_ProductProductImageFiles_ApplicationFiles_ProductImageFileId",
+                        column: x => x.ProductImageFileId,
+                        principalTable: "ApplicationFiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductProductImageFiles_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductProductImageFiles_ProductImageFileId",
+                table: "ProductProductImageFiles",
+                column: "ProductImageFileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductProductImageFiles");
+
+            migrationBuilder.CreateTable(
+                name: "ProductProductImageFile",
+                columns: table => new
+                {
+                    ProductImageFilesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductProductImageFile", x => new { x.ProductImageFilesId, x.ProductsId });
+                    table.ForeignKey(
+                        name: "FK_ProductProductImageFile_ApplicationFiles_ProductImageFilesId",
+                        column: x => x.ProductImageFilesId,
+                        principalTable: "ApplicationFiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductProductImageFile_Products_ProductsId",
+                        column: x => x.ProductsId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductProductImageFile_ProductsId",
+                table: "ProductProductImageFile",
+                column: "ProductsId");
+        }
+    }
+}
---
+++ Infrastructure/Mini-ECommerce.Persistence/Migrations/20240825120451_mig_10.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mini_ECommerce.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsMain",
+                table: "ProductProductImageFiles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsMain",
+                table: "ProductProductImageFiles",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+        }
+    }
+}
