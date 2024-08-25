c92241ec5c5581c095211bf28a563c94c91a1cb9
nurlancreus <nurlancreus007@gmail.com>
2024-08-25 21:57:49 +0400

    add new IBasketService interface and Basket DTOs

---
+++ Core/Mini-ECommerce.Application/Abstractions/Services/IBasketService.cs
@@ -0,0 +1,19 @@
+﻿using Mini_ECommerce.Application.DTOs.Basket;
+using Mini_ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Abstractions.Services
+{
+    public interface IBasketService
+    {
+        public Task<List<BasketItem>> GetBasketItemsAsync();
+        public Task AddItemToBasketAsync(CreateBasketItemDTO basketItem);
+        public Task UpdateQuantityAsync(UpdateBasketItemDTO basketItem);
+        public Task RemoveBasketItemAsync(string basketItemId);
+        public Basket? GetUserActiveBasket { get; }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/DTOs/Basket/CreateBasketItemDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.Basket
+{
+    public class CreateBasketItemDTO
+    {
+        // we could've demand BasketId from the client, but we won't. we will be add basketItem to the currently active basket. (which has no order yet)
+        // we don't tell client you have such baskets and that is the active one etc.
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
