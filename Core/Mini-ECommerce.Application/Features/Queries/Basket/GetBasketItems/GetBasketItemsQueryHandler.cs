a9d028824e739b7b56d24e7348d58e18e4476e36
nurlancreus <nurlancreus007@gmail.com>
2024-08-25 21:59:03 +0400

    add GetBasketItemsQuery

---
+++ Core/Mini-ECommerce.Application/Features/Queries/Basket/GetBasketItems/GetBasketItemsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Mini_ECommerce.Application.Abstractions.Services;
+using Mini_ECommerce.Application.DTOs.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Queries.Basket.GetBasketItems
+{
+    public class GetBasketItemsQueryHandler : IRequestHandler<GetBasketItemsQueryRequest, GetBasketItemsQueryResponse>
+    {
+        private readonly IBasketService _basketService;
+
+        public GetBasketItemsQueryHandler(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        public async Task<GetBasketItemsQueryResponse> Handle(GetBasketItemsQueryRequest request, CancellationToken cancellationToken)
+        {
+            var basketItems = await _basketService.GetBasketItemsAsync();
+
+            return new GetBasketItemsQueryResponse()
+            {
+                BasketItems = basketItems.Select(item => new GetBasketItemDTO()
+                {
+                    Name = item.Product.Name,
+                    Price = item.Product.Price,
+                    Quantity = item.Quantity,
+                }).ToList()
+            };
+        }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/Features/Queries/Basket/GetBasketItems/GetBasketItemsQueryResponse.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Mini_ECommerce.Application.DTOs.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Queries.Basket.GetBasketItems
+{
+    public class GetBasketItemsQueryResponse : IRequest<GetBasketItemsQueryRequest>
+    {
+        public ICollection<GetBasketItemDTO> BasketItems { get; set; } = [];
+    }
+}
