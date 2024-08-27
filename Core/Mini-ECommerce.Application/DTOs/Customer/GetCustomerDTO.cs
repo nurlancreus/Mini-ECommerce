fa48ced4d87103107951645ba0ab2de0c01f77e7
nurlancreus <nurlancreus007@gmail.com>
2024-08-27 22:09:10 +0400

    Modify User and Customer DTOs

---
+++ Core/Mini-ECommerce.Application/DTOs/Customer/GetCustomerDTO.cs
@@ -0,0 +1,15 @@
+﻿using Mini_ECommerce.Application.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.Customer
+{
+    public class GetCustomerDTO
+    {
+        string Id { get; set; }
+        public GetAppUserDTO AppUser { get; set; }
+    }
+}
