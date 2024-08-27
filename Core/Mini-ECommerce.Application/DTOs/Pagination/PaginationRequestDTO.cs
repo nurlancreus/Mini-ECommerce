91e988ca87e95cd631cc4b8cc8078a5e5aebce72
nurlancreus <nurlancreus007@gmail.com>
2024-08-27 22:08:39 +0400

    add new Pagination DTOs

---
+++ Core/Mini-ECommerce.Application/DTOs/Pagination/PaginationRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.Pagination
+{
+    public class PaginationRequestDTO
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+    }
+}
---
+++ Core/Mini-ECommerce.Application/DTOs/Pagination/PaginationResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using Mini_ECommerce.Domain.Entities.Base;
+using System.Linq;
+
+namespace Mini_ECommerce.Application.DTOs.Pagination
+{
+    public class PaginationResponseDTO<T> where T : BaseEntity
+    {
+        public int TotalItems { get; set; } // Total number of items
+        public int TotalPages { get; set; } // Total number of pages
+        public int Page { get; set; } // Current page number
+        public int PageSize { get; set; } // Number of items per page
+        public IQueryable<T> PaginatedQuery { get; set; } // The actual items for the current page
+
+        // Deconstruct method for easy unpacking
+        public void Deconstruct(out int totalItems, out int pageSize, out int page, out int totalPages, out IQueryable<T> paginatedQuery)
+        {
+            totalItems = TotalItems;
+            pageSize = PageSize;
+            page = Page;
+            totalPages = TotalPages;
+            paginatedQuery = PaginatedQuery;
+        }
+    }
+}
