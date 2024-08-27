203dac5e26644a1ef834a5a75c0ef8d264f7b798
nurlancreus <nurlancreus007@gmail.com>
2024-08-27 22:11:25 +0400

    Modify GetOrderByIdQueryhandler

--- Core/Mini-ECommerce.Application/Features/Queries/Order/GetOrderById/GetOrderByIdQueryHandler.cs
+++ Core/Mini-ECommerce.Application/Features/Queries/Order/GetOrderById/GetOrderByIdQueryHandler.cs
@@ -1,4 +1,8 @@
 ﻿using MediatR;
+using Mini_ECommerce.Application.Abstractions.Services;
+using Mini_ECommerce.Application.ViewModels.Address;
+using Mini_ECommerce.Application.ViewModels.Basket;
+using Mini_ECommerce.Application.ViewModels.Order;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +13,43 @@ namespace Mini_ECommerce.Application.Features.Queries.Order.GetOrderById
 {
     public class GetOrderByIdQueryHandler : IRequestHandler<GetOrderByIdQueryRequest, GetOrderByIdQueryResponse>
     {
-        public Task<GetOrderByIdQueryResponse> Handle(GetOrderByIdQueryRequest request, CancellationToken cancellationToken)
+        private readonly IOrderService _orderService;
+
+        public GetOrderByIdQueryHandler(IOrderService orderService)
         {
-            throw new NotImplementedException();
+            _orderService = orderService;
+        }
+
+        public async Task<GetOrderByIdQueryResponse> Handle(GetOrderByIdQueryRequest request, CancellationToken cancellationToken)
+        {
+            var order = await _orderService.GetOrderByIdAsync(request.Id);
+
+            return new GetOrderByIdQueryResponse()
+            {
+                Order = new GetOrderVM()
+                {
+                    Id = order.Id,
+                    CreatedAt = order.CreatedAt,
+                    Description = order.Description,
+                    OrderCode = order.OrderCode,
+                    TotalPrice = order.TotalPrice,
+                    Address = new GetAddressVM()
+                    {
+                        Id = order.Address.Id,
+                        Country = order.Address.Country,
+                        City = order.Address.City,
+                        State = order.Address.State,
+                        Street = order.Address.Street,
+                    },
+                    BasketItems = order.BasketItems.Select(bi => new GetBasketItemVM()
+                    {
+                        Name = bi.Name,
+                        Price = bi.Price,
+                        Quantity = bi.Quantity,
+                        TotalPrice = bi.TotalPrice,
+                    }).ToList()
+                }
+            };
         }
     }
 }
