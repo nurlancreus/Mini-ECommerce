e4971de2cd10d24253140d771fd1fa89f2cf1615
nurlancreus <nurlancreus007@gmail.com>
2024-08-28 23:58:54 +0400

    implement reset password commands

---
+++ Core/Mini-ECommerce.Application/Features/Commands/AppUser/ResetPassword/ResetPasswordCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Mini_ECommerce.Application.Abstractions.Services.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Commands.AppUser.ResetPassword
+{
+    public class ResetPasswordCommandHandler : IRequestHandler<ResetPasswordCommandRequest, ResetPasswordCommandResponse>
+    {
+        private readonly IAuthService _authService;
+
+        public ResetPasswordCommandHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<ResetPasswordCommandResponse> Handle(ResetPasswordCommandRequest request, CancellationToken cancellationToken)
+        {
+            await _authService.ResetPasswordAsnyc(request.Email);
+
+            return new ResetPasswordCommandResponse()
+            {
+                Success = true,
+            };
+        }
+    }
+}
