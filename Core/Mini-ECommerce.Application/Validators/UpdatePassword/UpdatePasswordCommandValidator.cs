71187a229ee4ac614418748a5b933a81ef49174e
nurlancreus <nurlancreus007@gmail.com>
2024-08-28 23:59:36 +0400

    add updatepasswordcommand validator

---
+++ Core/Mini-ECommerce.Application/Validators/UpdatePassword/UpdatePasswordCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Mini_ECommerce.Application.Features.Commands.AppUser.UpdatePassword;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Validators.UpdatePassword
+{
+    public class UpdatePasswordCommandValidator : AbstractValidator<UpdatePasswordCommandRequest>
+    {
+        public UpdatePasswordCommandValidator()
+        {
+            RuleFor(u => u.ConfirmPassword)
+                .Equal(u => u.Password).WithMessage("Passwords do not match.");
+        }
+    }
+}
