8ced5f48498f1b6d3a99d09d6e7494caf1bb10aa
nurlancreus <nurlancreus007@gmail.com>
2024-08-31 21:08:57 +0400

    add new GetRoleVM

---
+++ Core/Mini-ECommerce.Application/ViewModels/Role/GetRoleVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.ViewModels.Role
+{
+    public class GetRoleVM
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
