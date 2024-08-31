583bba05954056a826ab189264b06cedc56aea31
nurlancreus <nurlancreus007@gmail.com>
2024-08-31 14:32:44 +0400

    define HTTPMethods enum

---
+++ Core/Mini-ECommerce.Application/Enums/MethodType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Enums
+{
+    public enum MethodType
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE,
+        PATCH,
+        HEAD,
+        OPTIONS,
+        TRACE,
+        CONNECT
+    }
+}
