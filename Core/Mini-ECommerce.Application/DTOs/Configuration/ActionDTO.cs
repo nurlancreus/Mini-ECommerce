cab77673630026df26e34a36f114ae635b0bda3f
nurlancreus <nurlancreus007@gmail.com>
2024-08-31 14:32:13 +0400

    Defining related DTOs

---
+++ Core/Mini-ECommerce.Application/DTOs/Configuration/ActionDTO.cs
@@ -0,0 +1,17 @@
+﻿using Mini_ECommerce.Application.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.Configuration
+{
+    public class ActionDTO
+    {
+        public string ActionType { get; set; }
+        public string Method { get; set; }
+        public string Definition { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/DTOs/Configuration/MenuDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.Configuration
+{
+    public class MenuDTO
+    {
+        public string Name { get; set; }
+        public List<ActionDTO> Actions { get; set; } = [];
+    }
+}
