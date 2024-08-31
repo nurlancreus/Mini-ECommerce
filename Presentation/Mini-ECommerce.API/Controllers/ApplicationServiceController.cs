600e9426dee1008966cb221017dd9265667899ab
nurlancreus <nurlancreus007@gmail.com>
2024-08-31 14:32:56 +0400

    add ApplicationController

---
+++ Presentation/Mini-ECommerce.API/Controllers/ApplicationServiceController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mini_ECommerce.Application.Abstractions.Services.Application;
+
+namespace Mini_ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationServiceController : ControllerBase
+    {
+        private readonly IApplicationService _applicationService;
+
+        public ApplicationServiceController(IApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var datas = _applicationService.GetAuthorizeDefinitionEndpoints(typeof(Program));
+
+            return Ok(datas);
+        }
+    }
+}
