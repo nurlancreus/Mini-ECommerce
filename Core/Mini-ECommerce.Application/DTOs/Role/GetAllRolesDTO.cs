ea2a2f05fee66895a95709163f60bf2c4dccd2ac
nurlancreus <nurlancreus007@gmail.com>
2024-08-31 21:08:14 +0400

    add new RoleDTOs

---
+++ Core/Mini-ECommerce.Application/DTOs/Role/GetAllRolesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.Role
+{
+    public class GetAllRolesDTO
+    {
+        public List<GetRoleDTO> Roles { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/DTOs/Role/GetRoleDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.Role
+{
+    public class GetRoleDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
