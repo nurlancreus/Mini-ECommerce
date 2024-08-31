45895605bcf0496fe924d6c1076a6ad45aedc365
nurlancreus <nurlancreus007@gmail.com>
2024-08-31 14:31:03 +0400

    adding customer authorizeDefinition attribute and its enums

---
+++ Core/Mini-ECommerce.Application/Attributes/AuthorizeDefinitionAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Mini_ECommerce.Application.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Attributes
+{
+    public class AuthorizeDefinitionAttribute : Attribute
+    {
+        public AuthorizedMenu Menu { get; set; }
+        public string Definition { get; set; }
+        public ActionType ActionType { get; set; }
+
+    }
+}
---
+++ Core/Mini-ECommerce.Application/Enums/AuthorizedMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Enums
+{
+    public enum AuthorizedMenu
+    {
+        Products,
+        Baskets,
+        Orders,
+    }
+}
