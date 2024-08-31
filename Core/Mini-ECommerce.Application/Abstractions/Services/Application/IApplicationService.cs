8d37d4943fa65dbf73ffddccdc91238cea22ffdc
nurlancreus <nurlancreus007@gmail.com>
2024-08-31 14:32:02 +0400

    implement IApplication service and its GetCustomerAuthAttribute applied endpoints method

---
+++ Core/Mini-ECommerce.Application/Abstractions/Services/Application/IApplicationService.cs
@@ -0,0 +1,14 @@
+﻿using Mini_ECommerce.Application.DTOs.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Abstractions.Services.Application
+{
+    public interface IApplicationService
+    {
+        List<MenuDTO> GetAuthorizeDefinitionEndpoints(Type type);
+    }
+}
