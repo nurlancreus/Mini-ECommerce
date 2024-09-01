2bed686b1a5911ce2ac91239fc50539e2584d373
nurlancreus <nurlancreus007@gmail.com>
2024-09-01 21:32:37 +0400

    Add DTO and VM models for AuthEndpoint

---
+++ Core/Mini-ECommerce.Application/DTOs/AuthEndpoint/GetAuthEndpointDTO.cs
@@ -0,0 +1,15 @@
+﻿using Mini_ECommerce.Application.DTOs.Role;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.AuthEndpoint
+{
+    public class GetAuthEndpointDTO
+    {
+        public List<GetRoleDTO> Roles { get; set; } = [];
+        public string Code { get; set; }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/ViewModels/AuthEndpoint/GetAuthEndpointVM.cs
@@ -0,0 +1,16 @@
+﻿using Mini_ECommerce.Application.DTOs.Role;
+using Mini_ECommerce.Application.ViewModels.Role;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.ViewModels.AuthEndpoint
+{
+    public class GetAuthEndpointVM
+    {
+        public List<GetRoleVM> Roles { get; set; } = [];
+        public string Code { get; set; }
+    }
+}
