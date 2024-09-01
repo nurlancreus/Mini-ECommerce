12f08c38618dc053ad7896f8b23e756120018f46
nurlancreus <nurlancreus007@gmail.com>
2024-09-01 19:32:12 +0400

    adding IAuthEndpointRepos and implement them

---
+++ Infrastructure/Mini-ECommerce.Persistence/Concretes/Repositories/AppEndpoint/AppEndpointWriteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Mini_ECommerce.Application.Abstractions.Repositories;
+using Mini_ECommerce.Domain.Entities;
+using Mini_ECommerce.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Persistence.Concretes.Repositories
+{
+    public class AppEndpointWriteRepository : WriteRepository<AppEndpoint>, IAppEndpointWriteRepository
+    {
+        public AppEndpointWriteRepository(MiniECommerceDbContext context) : base(context)
+        {
+        }
+    }
+}
