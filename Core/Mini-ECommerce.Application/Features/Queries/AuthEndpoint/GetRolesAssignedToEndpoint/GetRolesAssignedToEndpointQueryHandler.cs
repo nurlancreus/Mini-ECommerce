7a102bb5df5c3417d886388507af5b851a48b34f
nurlancreus <nurlancreus007@gmail.com>
2024-09-01 19:34:06 +0400

    adding auth enpoint queries

---
+++ Core/Mini-ECommerce.Application/Features/Queries/AuthEndpoint/GetRolesAssignedToEndpoint/GetRolesAssignedToEndpointQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Mini_ECommerce.Application.Abstractions.Services;
+using Mini_ECommerce.Application.ViewModels.Role;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Queries.AuthEndpoint.GetRolesAssignedToEndpoint
+{
+    public class GetRolesAssignedToEndpointQueryHandler : IRequestHandler<GetRolesAssignedToEndpointQueryRequest, GetRolesAssignedToEndpointQueryResponse>
+    {
+        private readonly IAuthEndpointService _authEndpointService;
+
+        public GetRolesAssignedToEndpointQueryHandler(IAuthEndpointService authEndpointService)
+        {
+            _authEndpointService = authEndpointService;
+        }
+
+        public async Task<GetRolesAssignedToEndpointQueryResponse> Handle(GetRolesAssignedToEndpointQueryRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _authEndpointService.GetRolesToEndpointAsync(request.Code, request.Menu);
+
+            return new GetRolesAssignedToEndpointQueryResponse()
+            {
+                Success = true,
+                Roles = result.Select(r => new GetRoleVM()
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                }).ToList(),
+            };
+        }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/Features/Queries/AuthEndpoint/GetRolesAssignedToEndpoint/GetRolesAssignedToEndpointQueryRequest.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Queries.AuthEndpoint.GetRolesAssignedToEndpoint
+{
+    public class GetRolesAssignedToEndpointQueryRequest : IRequest<GetRolesAssignedToEndpointQueryResponse>
+    {
+        public string Code { get; set; }
+        public string Menu { get; set; }
+    }
+}
