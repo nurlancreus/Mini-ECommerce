d16c20dda9dc3fb767fc1b88684faadfa9b0b2ef
nurlancreus <nurlancreus007@gmail.com>
2024-09-01 19:32:45 +0400

    modify DTOs

--- Core/Mini-ECommerce.Application/DTOs/Pagination/PaginationResponseDTO.cs
+++ Core/Mini-ECommerce.Application/DTOs/Pagination/PaginationResponseDTO.cs
@@ -9,7 +9,7 @@ namespace Mini_ECommerce.Application.DTOs.Pagination
         public int TotalPages { get; set; } // Total number of pages
         public int Page { get; set; } // Current page number
         public int PageSize { get; set; } // Number of items per page
-        public IQueryable<T> PaginatedQuery { get; set; } // The actual items for the current page
+        public IQueryable<T>? PaginatedQuery { get; set; } // The actual items for the current page
 
         // Deconstruct method for easy unpacking
         public void Deconstruct(out int totalItems, out int pageSize, out int page, out int totalPages, out IQueryable<T> paginatedQuery)
