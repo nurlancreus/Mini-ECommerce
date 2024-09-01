48727c3a048a28e62424617e8c54778e970dfe7c
nurlancreus <nurlancreus007@gmail.com>
2024-09-01 19:33:52 +0400

    adding authendpoin commands

---
+++ Core/Mini-ECommerce.Application/Features/Commands/AuthEndpoint/AssignRoleEndpoint/AssignRoleEndpointCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Mini_ECommerce.Application.Abstractions.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Commands.AuthEndpoint.AssignRoleEndpoint
+{
+    public class AssignRoleEndpointCommandHandler : IRequestHandler<AssignRoleEndpointCommandRequest, AssignRoleEndpointCommandResponse>
+    {
+        private readonly IAuthEndpointService _authEndpointService;
+
+        public AssignRoleEndpointCommandHandler(IAuthEndpointService authEndpointService)
+        {
+            _authEndpointService = authEndpointService;
+        }
+
+        public async Task<AssignRoleEndpointCommandResponse> Handle(AssignRoleEndpointCommandRequest request, CancellationToken cancellationToken)
+        {
+            var endpointCode = await _authEndpointService.AssignRoleEndpointAsync(request.Roles, request.Menu, request.Code, request.Type!);
+
+            return new AssignRoleEndpointCommandResponse()
+            {
+                Success = true,
+                Message = $"Roles have been added to the endpoint '{endpointCode}'"
+            };
+        }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/Features/Commands/AuthEndpoint/AssignRoleEndpoint/AssignRoleEndpointCommandResponse.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Commands.AuthEndpoint.AssignRoleEndpoint
+{
+    public class AssignRoleEndpointCommandResponse : IRequest<AssignRoleEndpointCommandRequest>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+    }
+}
