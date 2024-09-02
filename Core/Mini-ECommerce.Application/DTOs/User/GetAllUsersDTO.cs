4a284823d30eb43b60ac6e8484ec45c2db8e2524
nurlancreus <nurlancreus007@gmail.com>
2024-09-02 10:54:40 +0400

    Add new GetAllUsersDTO

---
+++ Core/Mini-ECommerce.Application/DTOs/User/GetAllUsersDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.DTOs.User
+{
+    public class GetAllUsersDTO
+    {
+        public int TotalUserCount { get; set; }
+        public int TotalPagesCount { get; set; }
+        public int CurrentPage { get; set; }
+        public List<GetAppUserDTO> Users { get; set; } = [];
+    }
+}
