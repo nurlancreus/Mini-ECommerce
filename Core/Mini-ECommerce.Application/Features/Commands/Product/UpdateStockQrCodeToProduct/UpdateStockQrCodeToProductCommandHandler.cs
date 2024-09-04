18585acbad4bc9cb0e0e145de2ab5af62e7c8fad
nurlancreus <nurlancreus007@gmail.com>
2024-09-04 15:20:47 +0400

    add QrCode commands

---
+++ Core/Mini-ECommerce.Application/Features/Commands/Product/UpdateStockQrCodeToProduct/UpdateStockQrCodeToProductCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Mini_ECommerce.Application.Abstractions.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Commands.Product.UpdateStockQrCodeToProduct
+{
+    public class UpdateStockQrCodeToProductCommandHandler : IRequestHandler<UpdateStockQrCodeToProductCommandRequest, UpdateStockQrCodeToProductCommandResponse>
+    {
+        private readonly IQRCodeService _qRCodeService;
+        private readonly IProductService _productService;
+
+        public UpdateStockQrCodeToProductCommandHandler(IQRCodeService qRCodeService, IProductService productService)
+        {
+            _qRCodeService = qRCodeService;
+            _productService = productService;
+        }
+
+        public Task<UpdateStockQrCodeToProductCommandResponse> Handle(UpdateStockQrCodeToProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Mini-ECommerce.Application/Features/Commands/Product/UpdateStockQrCodeToProduct/UpdateStockQrCodeToProductCommandResponse.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Mini_ECommerce.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Features.Commands.Product.UpdateStockQrCodeToProduct
+{
+    public class UpdateStockQrCodeToProductCommandResponse : BaseResponse, IRequest<UpdateStockQrCodeToProductCommandRequest>
+    {
+    }
+}
