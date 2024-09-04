a12146063439e4a09a3206eb085e97af1d88d0f2
nurlancreus <nurlancreus007@gmail.com>
2024-09-04 15:17:52 +0400

    creating product and qrdCode services

---
+++ Core/Mini-ECommerce.Application/Abstractions/Services/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Mini_ECommerce.Application.DTOs.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Abstractions.Services
+{
+    public interface IProductService
+    {
+        Task CreateProductAsync(CreateProductDTO createProductDTO);
+        Task UpdateProductAsync(UpdateProductDTO updateProductDTO);
+        Task DeleteProductAsync(string id);
+        Task<byte[]> QrCodeToProductAsync(string productId);
+        Task StockUpdateToProductAsync(string productId, int stock);
+    }
+}
