ba6310613fa38f8546924e67ef4ed822a5a519d5
nurlancreus <nurlancreus007@gmail.com>
2024-09-05 15:25:08 +0400

    implement IFileService

--- Core/Mini-ECommerce.Application/Abstractions/Services/IFileService.cs
+++ Core/Mini-ECommerce.Application/Abstractions/Services/IFileService.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Mini_ECommerce.Application.Abstractions.Repositories;
+using Mini_ECommerce.Application.DTOs.File;
+using Mini_ECommerce.Domain.Entities;
+using Mini_ECommerce.Domain.Entities.Base;
+using Mini_ECommerce.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +14,9 @@ namespace Mini_ECommerce.Application.Abstractions.Services
 {
     public interface IFileService
     {
-        Task<List<(string fileName, string path)>> UploadAsync (string path, IFormFileCollection formFiles);
-        Task<bool> CopyFileAsync(string path, IFormFile formFile);
+        public Task UploadAsync<T>(string pathName, FormFileCollection formFiles, IWriteRepository<T> fileWriteRepository, Func<string, string, StorageType, bool> addFile) where T : AppFile;
+        public Task DeleteAsync<T>(string id, IWriteRepository<T> fileWriteRepository, IReadRepository<T> readRepository) where T : AppFile;
+        public Task<GetAppFilesDTO> GetFilesAsync<T>(int page, int size, string? pathName, IReadRepository<T> fileReadRepository) where T : AppFile;
+        public Task<GetAppFileDTO> GetAsync<T>(string id, IReadRepository<T> fileReadRepository) where T : AppFile;
     }
 }
