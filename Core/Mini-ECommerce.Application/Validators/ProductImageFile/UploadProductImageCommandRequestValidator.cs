5141180cb2c9734b7ffff9215bd0d773f185f904
nurlancreus <nurlancreus007@gmail.com>
2024-09-06 16:41:48 +0400

    adding upload product image validator

---
+++ Core/Mini-ECommerce.Application/Validators/ProductImageFile/UploadProductImageCommandRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Mini_ECommerce.Application.Features.Commands.ProductImageFile.UploadProductImage;
+using Mini_ECommerce.Application.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_ECommerce.Application.Validators.ProductImageFile
+{
+    public class UploadProductImageCommandRequestValidator : AbstractValidator<UploadProductImageCommandRequest>
+    {
+        private const int MaxFileSizeMB = 3;
+        public UploadProductImageCommandRequestValidator()
+        {
+
+            RuleForEach(p => p.ProductImages)
+               .NotNull()
+                   .WithMessage("Each product image is required.")
+               .Must(file => file.IsSizeOk(MaxFileSizeMB))
+                   .WithMessage($"File size should not exceed {MaxFileSizeMB} MB.")
+               .Must(file => file.RestrictExtension([".jpg", ".png", ".gif"]))
+                   .WithMessage("Only .jpg, .png, and .gif files are allowed.")
+               .Must(file => file.RestrictMimeTypes(["image/jpeg", "image/png", "image/gif"]))
+                   .WithMessage("Only image files (JPEG, PNG, GIF) are allowed.");
+        }
+    }
+}
